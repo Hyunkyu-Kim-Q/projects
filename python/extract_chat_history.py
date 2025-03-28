72e9d62c150aa7ebe501f4d788dec53a622f68c1
H.Kim <81006518+Hyunkyu-Kim-Q@users.noreply.github.com>
2025-03-28 12:32:14 +0800

    Update extract_chat_history.py

--- python/extract_chat_history.py
+++ python/extract_chat_history.py
@@ -1,46 +1,4 @@
 #!/usr/bin/python3
-
-"""
-
-There were few prerequisites to be able to run this.
-
-1. Selenium webdriver - Selenium will be used to extract chat history from the ChatGPT web UI.
-2. Google Chrome - ChatGPT will run on Google Chrome.
-3. BeautifulSoup4 - BeautifulSoup will be used to parse HTML and save the chat into a file.
-
-When I was running this in Ubuntu Linux which was running into PEP 668, so I had to create a virtual environment to install selenium webdriver.
-
--Downloading python venv
-sudo apt install python3.12-venv
-
--Activating virtual environment
-python3 -m venv myenv
-source myenv/bin/activate
-
--Downloading selenium webdriver
-pip install selenium webdriver-manager
-
--Checking if selenium is running
-pip list
-
--Downloading Google Chrome
-sudo apt update
-sudo apt install google-chrome-stable
-
-or alternatively,
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt --fix-broken install
-
--Checking Google Chrome version
-google-chrome --version
-
--Downloading Beautifulsoup4
-python -m pip install beautifulsoup4
-
-"""
-
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
